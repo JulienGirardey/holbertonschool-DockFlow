c68af335946fb01348c647256b2f0c22a03b03a9
JulienGirardey <juliengirardey2@gmail.com>
2025-09-29 14:26:56 +0200

    feat: add API routes for authentication, document management, and settings

---
+++ docflow/app/api/auth/register/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+	//logique GET /api/register
+}
+
+export async function POST(req: NextRequest) {
+	//logique POST /api/register
+}
---
+++ docflow/app/api/documents/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+	//logique
+}
+
+export async function PUT(req: NextRequest) {
+	//logique
+}
+
+export async function DELETE(req: NextRequest) {
+	//logique
+}
